9a7c6c1cedd452c3c106c11c6c7c649f47b407fc
annaBalabak <annabalabak15@gmail.com>
2023-03-23 13:52:47 +0100

    Made a conversion celsius-fahrenheit

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -1,38 +1,34 @@
-import React from "react"
-import DateForm from "./DateForm"
+import React from "react";
+import DateForm from "./DateForm";
+import WeatherTemperature from "./WeatherTemperature";
 
-export default function WeatherData(props){
-    return(
-<div className="container">
-  <div className="main">
-    <h1>{props.data.city}</h1>
-    <h2>
-      <DateForm date={props.data.date} />
-    </h2>
-    <h3></h3>
-    <div className="temp-container">
-      <img src={props.data.icon} alt={props.data.description} width="100px" />
-      <span className="temperature">
-        {Math.round(props.data.temperature)}
-      </span>{" "}
-      <span className="units">
-        {" "}
-        <a className="active" href="#">
-          ° C
-        </a>{" "}
-        | <a href="#">° F</a>{" "}
-      </span>
+export default function WeatherData(props) {
+  return (
+    <div className="container">
+      <div className="main">
+        <h1>{props.data.city}</h1>
+        <h2>
+          <DateForm date={props.data.date} />
+        </h2>
+        <h3></h3>
+        <WeatherTemperature
+          celsius={props.data.temperature}
+          icon={props.data.icon}
+          description={props.data.description}
+        />
+        <div className="discription mt-3">
+          <p className="conditions">{props.data.description}</p>
+          <p className="windspeed">
+            Wind speed: {Math.round(props.data.wind)} km/h
+          </p>
+          <p className="humidity">Humidity: {props.data.humidity}%</p>
+          <p className="temperature-range">
+            Min <span className="min">{Math.round(props.data.min)}</span>° C /
+            Max <span className="max"> {Math.round(props.data.max)}</span>° C
+          </p>
+        </div>
+        <div></div>
+      </div>
     </div>
-    <div className="discription mt-3">
-      <p className="conditions">{props.data.description}</p>
-      <p className="windspeed">Wind speed: {Math.round(props.data.wind)} km/h</p>
-      <p className="humidity">Humidity: {props.data.humidity}%</p>
-      <p className="temperature-range">
-        Min <span className="min">{Math.round(props.data.min)}</span>° C / Max{" "}
-        <span className="max"> {Math.round(props.data.max)}</span>° C
-      </p>
-    </div>
-    <div></div>
-  </div>
-</div>
-)}
+  );
+}
---
+++ src/WeatherTemperature.js
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+
+export default function WeatherTemperature(props){
+  const [unit, setUnit] = useState("celsius");
+
+  function showCelsius(event){
+     event.preventDefault();
+     setUnit("celsius");
+  }
+  function showFahrenheit(event){
+    event.preventDefault();
+    setUnit("fahrenheit")
+  }
+  function fahrenheit(){
+    return (props.celsius *9) / 5 + 32
+  }
+  if (unit === "celsius"){
+    return (
+      <div className="WeatherTemperature">
+        <div className="temp-container">
+          <img className="icon"
+            src={props.icon}
+            alt={props.description}
+            width="100px"
+          />
+        
+        <span className="temperature">{Math.round(props.celsius)}</span>{" "}
+        <span className="units">
+          ° C |
+          <a href="/" onClick={showFahrenheit}>
+            ° F
+          </a>{" "}
+        </span>
+      </div>
+      </div>
+    );
+} else {
+  return (
+    <div className="WeatherTemperature">
+      <div className="temp-container">
+        <img className="icon" src={props.icon} alt={props.description} width="100px" />
+        <span className="temperature">{Math.round(fahrenheit())}</span>{" "}
+        <span className="units">
+          <a href="/" onClick={showCelsius}>
+            {" "}
+            ° C{" "}
+          </a>
+          | ° F{" "}
+        </span>
+      </div>
+    </div>
+  );
+}
+}
